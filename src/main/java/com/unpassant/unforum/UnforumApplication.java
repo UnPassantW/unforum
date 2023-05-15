27fb77588b5acc003cd0b42e3213747e17509ea1
你çUnPassant <1940097836@qq.com>
2023-05-15 15:26:13 +0800

    init repo

---
+++ src/main/java/com/unpassant/unforum/UnforumApplication.java
@@ -0,0 +1,13 @@
+package com.unpassant.unforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnforumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnforumApplication.class, args);
+    }
+
+}
